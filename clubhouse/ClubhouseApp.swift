6b727ac2b1b6568cb8027399f0fdc5cebee740ac
Hikikomori <91256870+developer-ongar@users.noreply.github.com>
2022-06-03 12:27:27 +0600

    Create ClubhouseApp.swift

---
+++ clubhouse/ClubhouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClubhouseApp.swift
+//  Clubhouse
+//
+//  Created by Ongar.dev on 02/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ClubhouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
